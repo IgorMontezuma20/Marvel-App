22fe8618a43648751770ebc089b360488434269f
Igor Montezuma <85948415+IgorMontezuma20@users.noreply.github.com>
2022-11-30 22:22:24 -0300

    ajustes no projeto

--- app/src/main/java/com/example/marvelapp2/ui/Test.kt
+++ app/src/main/java/com/example/marvelapp2/ui/Test.kt
@@ -2,6 +2,7 @@ package com.example.marvelapp2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvelapp2.R
 
 class Test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
